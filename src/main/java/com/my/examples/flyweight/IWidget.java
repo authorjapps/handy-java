bdf74cc200ee602e52e90a0a6192dcd7567aab6c
Nirmal Chandra <nirmal.fleet@gmail.com>
2017-07-06 23:20:27 +0100

    UMA # File Reader API/service

--- src/main/java/com/my/examples/flyweight/IWidget.java
+++
@@ -1,9 +0,0 @@
-package com.my.examples.flyweight;
-
-public interface IWidget {
-    public void setWidgetName(String name);
-    public String getWidgetName();
-    public void setWidgetContent(String content);
-    public void display();
-    public void applyTheme(String color,String postion,Layoutmanager manager);
-}
