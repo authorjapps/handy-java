514c844b890755f2d451339573b246b4a681aefc
Nirmal Chandra <nirmal.fleet@gmail.com>
2017-07-16 20:28:14 +0100

    HANDY-JAVA # Design patterns remoduled

---
+++ design-patterns/src/test/java/com/my/examples/jsontopojo/JsonToPojoTest.java
@@ -0,0 +1,13 @@
+package com.my.examples.jsontopojo;
+
+import org.junit.Test;
+
+public class JsonToPojoTest {
+    
+    @Test
+    public void testJson() throws Exception {
+        
+        // use: com.my.examples.jsontopojo.JsonToPojo.main()
+        
+    }
+}
